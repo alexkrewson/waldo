587e2521a29cc31745daf0973bfc673c9ad1c831
alexkrewson <alex.krewson@gmail.com>
2021-12-22 13:13:58 -0800

    updates for heroku deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root 'characters#index'
   resources :characters
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
